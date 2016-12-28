e4d09e8e8ac8553acbac1092bbafbb51fbd11b90
AnnaKazmierczak <annakazmierczak@onet.eu>
2016-12-28 21:58:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+
+
+
+console.log('hello');
+
+
+
+
+
+//ponizej - dziala na ham menu, ale jednoczesnie powoduje, ze przy normalnym menu znika nav// 
+
+
+//jQuery(function( $ ){
+//console.log('hello');
+//
+//$(".label-toggle").click(function () {
+//    $("#menu").show();
+//});
+//	
+//$("nav li").click(function () {
+//    $("#menu").hide();
+//});
+//	
+//	$(".label-toggle").click(function () {
+//    $("#menu").show();
+//});
+//	
+////$('#about_me').change(function() {
+////$('#menu').hide();
+////});
+//});
